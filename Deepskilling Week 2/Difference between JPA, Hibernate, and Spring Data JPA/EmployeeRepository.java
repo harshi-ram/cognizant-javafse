64d9db5106484bc528bdb5526bcafdb5815bbd5d
harshi-ram <143722804+harshi-ram@users.noreply.github.com>
2026-07-19 17:01:30 +0530

    Update EmployeeRepository.java

--- Deepskilling Week 2/Difference between JPA, Hibernate, and Spring Data JPA/EmployeeRepository.java
+++ Deepskilling Week 2/Difference between JPA, Hibernate, and Spring Data JPA/EmployeeRepository.java
@@ -1,3 +1,9 @@
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+package com.cognizant.orm_learn.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cognizant.orm_learn.model.Country;
+
+public interface CountryRepository extends JpaRepository<Country, String> {
 
 }
