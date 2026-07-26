6916072bd2ee884013f1d4195588adf41bf7a294
harshi-ram <143722804+harshi-ram@users.noreply.github.com>
2026-07-26 13:12:14 +0530

    Create Home.js

---
+++ Deepskilling Week 5/2. ReactJS-HOL/Home.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+
+import class Home extends Component{
+    render(){
+        <div>
+        <h3> Welcome to the Home Page of Student Management Portal </h3>
+        </div>
+    }
+}
