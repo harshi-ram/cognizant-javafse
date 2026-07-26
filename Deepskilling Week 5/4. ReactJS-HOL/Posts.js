d691bf103f11754a1f728827025f1612401d635c
harshi-ram <143722804+harshi-ram@users.noreply.github.com>
2026-07-27 00:06:17 +0530

    Create Posts.js

---
+++ Deepskilling Week 5/4. ReactJS-HOL/Posts.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import Post from './Post';
+
+class Posts extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: []
+        };
+    }
+
+    loadPosts() {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(data => {
+                const formattedPosts = data.map( //mapping the fetched GET data
+                    item => new Post(item.id, item.title, item.body)
+                );
+                this.setState({ posts: formattedPosts });
+            })
+            .catch(error => {
+                console.error("Error fetching posts:", error);
+            });
+    }
+
+    componentDidMount() {
+        this.loadPosts();
+    }
+
+    componentDidCatch(error, info) {
+        alert("An error occurred: " + error);
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Blog Posts</h2>
+                {this.state.posts.map(post => (
+                    <div key={post.id}>
+                        <h3>{post.title}</h3>
+                        <p>{post.body}</p>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+}
+
+export default Posts;
