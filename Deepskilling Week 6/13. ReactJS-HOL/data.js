8ec436b1c8ba7bdfa71992a3779bfd4d2c934fb6
harshi-ram <143722804+harshi-ram@users.noreply.github.com>
2026-07-27 11:17:40 +0530

    Create data.js

---
+++ Deepskilling Week 6/13. ReactJS-HOL/data.js
@@ -0,0 +1,16 @@
+export const books = [
+  { id: 101, bname: 'Master React', price: 670 },
+  { id: 102, bname: 'Deep Dive into Angular 11', price: 800 },
+  { id: 103, bname: 'Mongo Essentials', price: 450 },
+];
+
+export const blogs = {
+  title: 'React Learning Guide',
+  author: 'John Doe',
+  isPublished: true,
+};
+
+export const courses = [
+  { id: 1, cname: 'React JS', rating: '4.8' },
+  { id: 2, cname: 'Node JS', rating: '4.5' },
+];
